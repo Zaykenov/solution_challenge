0e9f262d4bfe06682c764ac4ea3d6583502e6518
Yernur Beisenbek <kolbar33@gmail.com>
2023-05-27 22:38:16 +0600

    fixed import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:solution_challenge/presentation_UI/screens/map_page.dart';
-import 'package:solution_challenge/presentation_UI/screens/onboarding_page.dart';
 import 'package:solution_challenge/presentation_UI/screens/information_page.dart';
 import 'package:solution_challenge/constants/colors.dart';
 import 'package:solution_challenge/presentation_UI/screens/drugsearch_page.dart';
